298c94c1e41938ee11ca60d1c5d56f0fbf81f5a0
Alexandru Tenche <alex.tenche@gmail.com>
2020-04-20 23:29:40 +0300

    added decorator first example

---
+++ decorator_ex1/src/main/java/model/Beverage.java
@@ -0,0 +1,13 @@
+package model;
+
+public abstract class Beverage {
+
+    String description = "unknown beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+
+}
