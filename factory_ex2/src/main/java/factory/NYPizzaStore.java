765379ce3666439492eb91434188b60a525609a7
Alexandru Tenche <alex.tenche@gmail.com>
2020-04-25 17:27:53 +0300

    added factory example 2

---
+++ factory_ex2/src/main/java/factory/NYPizzaStore.java
@@ -0,0 +1,21 @@
+package factory;
+
+public class NYPizzaStore extends PizzaStore {
+
+    @Override
+    Pizza createPizza(String item) {
+        switch (item) {
+            case "cheese":
+                return new NYStyleCheesePizza();
+            case "veggie":
+                return new NYStyleVeggiePizza();
+            case "clam":
+                return new NYStyleClamPizza();
+            case "pepperoni":
+                return new NYStylePepperoniPizza();
+            default:
+                return null;
+        }
+    }
+
+}
